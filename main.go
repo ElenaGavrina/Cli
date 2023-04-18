e0fc9717c101a55ad0cbc2aedd26929ae94487ba
ElenaGavrina <45360118+ElenaGavrina@users.noreply.github.com>
2023-04-18 18:39:30 +0300

    Update main.go

--- main.go
+++ main.go
@@ -11,18 +11,17 @@ func main (){
 	reader := bufio.NewReader(os.Stdin)
 	m, err := reader.ReadString('\n')
 	if err != nil {
-    	fmt.Println(err)
+    	    fmt.Println(err)
   	}
 	fmt.Print(isPalindrom(m))
-
 }	
 
 func isPalindrom (some string) bool {
-	l :=strings.TrimSpace(strings.ToLower(some))
+    l :=strings.TrimSpace(strings.ToLower(some))
     for i:=0; i < len(l)/2; i++ {
     	if (l[i] != l[len(l) -1 - i])  {
-    		return false
+    	    return false
     	}
-  	}
-  	return true
+    }
+    return true
 }
