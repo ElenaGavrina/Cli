579dd3e07693cb1f768a173719506586c6b766ab
ElenaGavrina <45360118+ElenaGavrina@users.noreply.github.com>
2023-04-18 18:38:13 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"bufio"
-	"os"
-	"strings"
-
+  "bufio"
+  "fmt"
+  "os"
+  "strings"
 )
 
 func main (){
 	reader := bufio.NewReader(os.Stdin)
-	m, _ := reader.ReadString('\n')
-	fmt.Println(isPalindrom(string(m)))
+	m, err := reader.ReadString('\n')
+	if err != nil {
+    	fmt.Println(err)
+  	}
+	fmt.Print(isPalindrom(m))
 
-}
+}	
 
-func isPalindrom (some string) string {
-	l :=strings.ToLower(some)
-	for i:=0; i < len(l)/2; i++ {
-		if (l[i] != l[len(l) -1 - i] ) {
-			return "This isn't a palindrome"
-		}
-	}	
-	return "This is a palindrome"
+func isPalindrom (some string) bool {
+	l :=strings.TrimSpace(strings.ToLower(some))
+    for i:=0; i < len(l)/2; i++ {
+    	if (l[i] != l[len(l) -1 - i])  {
+    		return false
+    	}
+  	}
+  	return true
 }
